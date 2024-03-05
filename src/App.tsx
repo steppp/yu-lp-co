0bf8d3f3823951fb2e8351c1bd20da748e1ca319
Stefano Andriolo <dev@srand.it>
2024-03-05 11:43:31 +0100

    Add navbar, add fontawesome, minor changes
    Add a very basic navbar, minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import LpCountersArea from './components/LpCountersArea'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <LpCountersArea />
       <div style={{ height: '300px' }} />
     </>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,5 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import './style.css'
+
 function Navbar() {
-  return <nav></nav>
+  return <nav className="navbarWrapper">
+    <div className='navbarContainer'>
+      <div className='navbarLogo'>
+        <span className='logo'>
+          YU-LP-CO
+        </span>
+      </div>
+      <div className='rightWidget'>
+        <FontAwesomeIcon icon={faBars} />
+      </div>
+    </div>
+  </nav>
 }
 
 export default Navbar
